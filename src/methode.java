4c2d061fefcb6162edbfaba29017e7d439c63a97
YacineBoukliff <yacine69800@live.fr>
2025-01-12 09:32:19 +0100

    Fonction en Java

---
+++ src/methode.java
@@ -0,0 +1,15 @@
+public class methode {
+    public static void main(String[] args) {
+        String nom = "yacine";
+      
+        AfficherPrenom(nom);
+        AfficherPrenom(nom);
+        AfficherPrenom(nom);
+    }
+
+    static void AfficherPrenom(String nom){
+        System.out.println("Bonjour " + nom + ".");
+    }
+
+    
+}
